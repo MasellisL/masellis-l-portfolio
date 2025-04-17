5c8e4069a256737fe560f39636b5137b12466f0e
Leo Masellis <leomasellis@gmail.com>
2025-04-17 11:12:07 -0700

    seo updates and adding semantic html

--- pages/scheduler.jsx
+++ pages/scheduler.jsx
@@ -8,31 +8,24 @@ import nestWorkTutorial from '/nest-work-tutorial.png';
 function Gamecube() {
     return(
         <div className="main-gc-content">
-            <Link to="/"><img className="back-button" src={backButton}></img></Link>
+            <Link to="/"><img className="back-button" alt="back button icon" src={backButton}></img></Link>
             <div className="project-images-container">
                 <div className="image-and-text">
-                <img className="project-image" src={nestWorkHome}></img>
+                <img className="project-image" src={nestWorkHome} alt="React nestWork Home Page"></img>
                 <div className="img-text-container">
                     <h1 className="img-text">Overview page</h1>
                     <h2 className="img-text">Page for selecting which interns will be included in the pairing algorithm.</h2>
                 </div>
                 </div>
                 <div className="image-and-text">
-                <img className="project-image" src={nestWorkResults}></img>
+                <img className="project-image" src={nestWorkResults} alt="React nestWork results Page"></img>
                 <div className="img-text-container">
                     <h1 className="img-text">Results</h1>
                     <h2 className="img-text">Page for viewing the pairings that have been created, with the ability to download as a csv file, and edit pairings.</h2>
                 </div>
                 </div>
                 <div className="image-and-text">
-                <img className="project-image" src={nestWorkTutorial}></img>
-                <div className="img-text-container">
-                    <h1 className="img-text">Tutorial Modal</h1>
-                    <h2 className="img-text">Tutorial modal for first time users.</h2>
-                </div>
-                </div>
-                <div className="image-and-text">
-                <img className="project-image" src={nestWorkTutorial}></img>
+                <img className="project-image" src={nestWorkTutorial} alt="React nestwork tutorial page"></img>
                 <div className="img-text-container">
                     <h1 className="img-text">Tutorial Modal</h1>
                     <h2 className="img-text">Tutorial modal for first time users.</h2>
--- pages/typing.jsx
+++ pages/typing.jsx
@@ -6,10 +6,10 @@ import typingPage2 from '/typing2.png'
 function Typing() {
     return(
         <div className="main-gc-content">
-            <Link to="/"><img className="back-button" src={backButton}></img></Link>
+            <Link to="/"><img className="back-button"  alt="backButton" src={backButton}></img></Link>
             <div className="project-images-container">
                 <div className="image-and-text">
-                <img className="project-image" src={typingPage}></img>
+                <img className="project-image" alt="javascript main page image" src={typingPage}></img>
                 <div className="img-text-container">
                     <h1 className="img-text">Typing Game</h1>
                     <h2 className="img-text">Simple JavaScript web game. Characters will change to green or red depending on if the input is correct or incorrect.</h2>
@@ -18,7 +18,7 @@ function Typing() {
             </div>
             <div className="project-images-container">
                 <div className="image-and-text">
-                <img className="project-image" src={typingPage2}></img>
+                <img className="project-image" alt="javaScript typing page image" src={typingPage2}></img>
                 <div className="img-text-container">
                     <h1 className="img-text">End Screen</h1>
                     <h2 className="img-text">After the 60 seconds, the timer stops, and the text area locks. You can then view your final stats, and refresh to try again.</h2>
