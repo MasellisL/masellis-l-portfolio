98d52dc8070bf4dd1c2b41992d4d4be90c874200
Leo Masellis <leomasellis@gmail.com>
2025-04-30 16:29:50 -0700

    finished top section

--- pages/home-page/home.jsx
+++ pages/home-page/home.jsx
@@ -1,4 +1,5 @@
 import './home.css'
+import Leo from '../../src/assets/leoMasellis.png';
 // import {Link} from 'react-router-dom';
 // import gitHub from "/git-hub.png";
 // import linkedIn from "/linked-in.png";
@@ -7,12 +8,17 @@ import './home.css'
 
 function Home() {
     return(
+    <>
       <section className="introSection">
         <div className="nameAndOccupation">
           <p className="myName">Leo Masellis</p>
           <h1 className="myOccupation">Front End Web Developer</h1>
         </div>
+        <div className="leoImageContainer">
+          <img className="leoImage" alt="Image of Leo masellis" src={Leo}></img>
+        </div>
       </section>
+    </>  
   )
 }
 
